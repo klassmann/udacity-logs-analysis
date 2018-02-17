257eb3c51275f577a2e3a06f3d9d4998a16377e2
Lucas Klassmann <lucasklassmann@gmail.com>
2018-02-16 23:25:12 -0200

    top authors query

--- report.py
+++ report.py
@@ -38,7 +38,13 @@ class Report(object):
         self.db = Database(CONNECTION_STRING)
         
     def top_articles(self):
-        pass
+        query_top_articles = """
+        SELECT slug, qtd FROM articles a INNER JOIN 
+                (SELECT path, count(*) AS qtd FROM log GROUP BY path) AS l 
+                ON '/article/' || a.slug = l.path
+            ORDER BY qtd DESC
+            LIMIT 3;
+        """
 
     def top_authors(self):
         pass
